8fe8c00e56842f00cfc1e129d80e90e584d31115
valluri-bharathi <52210048+valluri-bharathi@users.noreply.github.com>
2020-04-23 10:09:25 +0530

    Add files via upload

---
+++ demandcarwash/src/main/java/com/example/demandcarwash/dao/CustomerDaoImpl.java
@@ -0,0 +1,54 @@
+package com.example.demandcarwash.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demandcarwash.entity.CustCarDetails;
+import com.example.demandcarwash.entity.CustomerDetails;
+import com.example.demandcarwash.exception.ProgramException;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao {
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	Query query = new Query();
+
+	@Override
+	public boolean findUser(CustomerDetails user) throws ProgramException {
+
+		String uName = user.getUserName();
+		String pwd = user.getPassword();
+		try {
+			
+			query = query.addCriteria(Criteria.where("userName").is(uName).andOperator(Criteria.where("password").is(pwd)));
+
+			boolean dataExists = mongoTemplate.exists(query, "custdetails");
+			if (dataExists) {
+				return true;
+			}
+		} catch (Exception e) {
+			throw new ProgramException("reposit error");
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean addDetails(CustCarDetails details) throws ProgramException {
+		
+		try {
+			CustCarDetails dataExists = mongoTemplate.insert(details, "custCarDetails");
+	
+			if(dataExists!=null) {
+				return true;
+			}
+		}catch(Exception e) {
+			throw new ProgramException("error occurred");
+		}
+		return false;
+	}
+
+}
---
+++ demandcarwash/src/main/java/com/example/demandcarwash/service/CustomerService.java
@@ -0,0 +1,13 @@
+package com.example.demandcarwash.service;
+
+import com.example.demandcarwash.entity.CustCarDetails;
+import com.example.demandcarwash.entity.CustomerDetails;
+import com.example.demandcarwash.exception.ProgramException;
+
+public interface CustomerService {
+
+	boolean login(CustomerDetails customer) throws ProgramException;
+	boolean addDetails(CustCarDetails details) throws ProgramException;
+
+
+}
